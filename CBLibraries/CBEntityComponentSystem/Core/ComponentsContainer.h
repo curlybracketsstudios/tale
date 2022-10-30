04923f2ff77d0859f6216faf1ab821296971e4e6
jan <willemsen_jan@yahoo.co.uk>
2022-10-30 20:46:41 +0100

    A ComponentsContainer should be constructable

---
+++ CBLibraries/CBEntityComponentSystem/Core/ComponentsContainer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CBEntityComponentSystem/CBEntityComponentSystemLibrary.h"
+
+namespace cb
+{
+	namespace ecs
+	{
+		class ComponentsContainer
+		{
+		public:
+CBENTITYCOMPONENTSYSTEMSHARED_EXPORT				ComponentsContainer();
+
+		};
+	}
+}
---
+++ CBLibraries/CBEntityComponentSystemTest/Core/ComponentsContainerTest.cpp
@@ -0,0 +1,21 @@
+#include "CBEntityComponentSystemTest/Core/TestComponents.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+//-------------------------------------------------------------------
+
+using namespace testing;
+
+//-------------------------------------------------------------------
+
+#include "CBEntityComponentSystem/Core/ComponentsContainer.h"
+
+#include <memory>
+
+//-------------------------------------------------------------------
+
+TEST(AComponentsContainer, ShouldBeConstructable)
+{
+	
+}
