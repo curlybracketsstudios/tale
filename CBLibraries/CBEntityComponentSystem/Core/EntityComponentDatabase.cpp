0e3c3d191eb0c18edecbd9a76b6cc095e9cc763b
jan <willemsen_jan@yahoo.co.uk>
2022-10-30 20:13:29 +0100

    An ECDatabase should return the correct pointer after the component was added to the entity and retrieved

--- CBLibraries/CBEntityComponentSystem/Core/EntityComponentDatabase.cpp
+++ CBLibraries/CBEntityComponentSystem/Core/EntityComponentDatabase.cpp
@@ -16,7 +16,7 @@ cb::ecs::Entity cb::ecs::EntityComponentDatabase::createEntity()
 {
 	cb::ecs::Entity entity;
 	entity.setGuid(cb::base::createGUID());
-	m_all_entities.insert(entity);
+	m_entity_components.insert(std::make_pair(entity, std::vector<std::shared_ptr<cb::ecs::Component> >()));
 	return entity;
 }
 
@@ -24,17 +24,17 @@ cb::ecs::Entity cb::ecs::EntityComponentDatabase::createEntity()
 
 bool cb::ecs::EntityComponentDatabase::hasEntity(const cb::ecs::Entity& entity) const
 {
-	return m_all_entities.find(entity) != m_all_entities.end();
+	return m_entity_components.find(entity) != m_entity_components.end();
 }
 
 //-------------------------------------------------------------------
 
 void cb::ecs::EntityComponentDatabase::removeEntity(const cb::ecs::Entity& entity)
 {
-	auto it_entity = m_all_entities.find(entity);
-	if (it_entity != m_all_entities.end())
+	auto it_entity = m_entity_components.find(entity);
+	if (it_entity != m_entity_components.end())
 	{
-		m_all_entities.erase(it_entity);
+		m_entity_components.erase(it_entity);
 	}
 }
 
--- CBLibraries/CBEntityComponentSystemTest/Core/ComponentCreationTest.cpp
+++ CBLibraries/CBEntityComponentSystemTest/Core/ComponentCreationTest.cpp
@@ -7,6 +7,7 @@ using namespace testing;
 
 //-------------------------------------------------------------------
 
+#include "CBEntityComponentSystem/Core/Component.h"
 #include "CBEntityComponentSystem/Core/Entity.h"
 #include "CBEntityComponentSystem/Core/EntityComponentDatabase.h"
 
@@ -14,8 +15,11 @@ using namespace testing;
 
 //-------------------------------------------------------------------
 
-class TestComponent
+class TestComponent : public cb::ecs::Component
 {
+public:
+			TestComponent() {};
+			~TestComponent() override = default;
 };
 
 //-------------------------------------------------------------------
@@ -28,6 +32,19 @@ TEST(AnEntityComponentDatabase, ShouldReturnAValidSharedPointerToAComponentOfThe
 	std::shared_ptr<TestComponent> test_component = database.addComponentToEntity<TestComponent>(entity);
 	
 	ASSERT_THAT(test_component, Ne(nullptr));
+}
+
+//-------------------------------------------------------------------
+
+TEST(AnEntityComponentDatabase, ShouldReturnTheSameComponentFromTheEntityWhenItWasPreviouslyAdded)
+{
+	cb::ecs::EntityComponentDatabase database;
+	cb::ecs::Entity entity = database.createEntity();
+	std::shared_ptr<TestComponent> test_component_added = database.addComponentToEntity<TestComponent>(entity);
+
+	std::shared_ptr<TestComponent> test_component_retrieved = database.getComponentFromEntity<TestComponent>(entity);
+
+	ASSERT_THAT(test_component_added, Eq(test_component_retrieved));
 }
 
 //-------------------------------------------------------------------
