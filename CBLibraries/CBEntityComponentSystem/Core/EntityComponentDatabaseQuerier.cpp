599805a60612ba6ba9a8ba6ad3d803bf59464d2f
jan <willemsen_jan@yahoo.co.uk>
2022-11-13 21:29:47 +0100

    ECDBQuerier constructor cleaned

--- CBLibraries/CBEntityComponentSystem/Core/EntityComponentDatabaseQuerier.cpp
+++ CBLibraries/CBEntityComponentSystem/Core/EntityComponentDatabaseQuerier.cpp
@@ -1,5 +1,8 @@
 #include "CBEntityComponentSystem/Core/EntityComponentDatabaseQuerier.h"
 
+#include "CBEntityComponentSystem/Core/Component/ComponentIndexGenerator.h"
+#include "CBEntityComponentSystem/Core/EntitiesComponentsContainer.h"
+
 //-------------------------------------------------------------------
 // CONSTRUCTOR - DESTRUCTOR
 //-------------------------------------------------------------------
@@ -9,8 +12,8 @@ cb::ecs::EntityComponentDatabaseQuerier::EntityComponentDatabaseQuerier
 	std::shared_ptr<cb::ecs::EntitiesComponentsContainer> entitiesComponentsContainer,
 	std::shared_ptr<cb::ecs::ComponentIndexGenerator> componentIndexGenerator
 )
-	: m_entities_components_container(entitiesComponentsContainer)
-	, m_component_index_generator(componentIndexGenerator)
+	: m_entities_components_container(entitiesComponentsContainer != nullptr ? entitiesComponentsContainer : std::make_shared<cb::ecs::EntitiesComponentsContainer>())
+	, m_component_index_generator(componentIndexGenerator != nullptr ? componentIndexGenerator : std::make_shared<cb::ecs::ComponentIndexGenerator>())
 {
 }
 
--- CBLibraries/CBEntityComponentSystem/Core/EntityComponentDatabaseQuerier.h
+++ CBLibraries/CBEntityComponentSystem/Core/EntityComponentDatabaseQuerier.h
@@ -2,20 +2,21 @@
 
 #include "CBEntityComponentSystem/CBEntityComponentSystemLibrary.h"
 
-#include "CBEntityComponentSystem/Core/Component/ComponentIndexGenerator.h"
-#include "CBEntityComponentSystem/Core/EntitiesComponentsContainer.h"
+#include <memory>
 
 namespace cb
 {
 	namespace ecs
 	{
+		class ComponentIndexGenerator;
 		class Entity;
+		class EntitiesComponentsContainer;
 		class EntityComponentDatabaseQuerier
 		{
 		public:
 			CBENTITYCOMPONENTSYSTEMSHARED_EXPORT													EntityComponentDatabaseQuerier(
-																										std::shared_ptr<cb::ecs::EntitiesComponentsContainer> entitiesComponentsContainer = std::make_shared<cb::ecs::EntitiesComponentsContainer>(),
-																										std::shared_ptr<cb::ecs::ComponentIndexGenerator> componentIndexGenerator = std::make_shared<cb::ecs::ComponentIndexGenerator>()
+																										std::shared_ptr<cb::ecs::EntitiesComponentsContainer> entitiesComponentsContainer = nullptr,
+																										std::shared_ptr<cb::ecs::ComponentIndexGenerator> componentIndexGenerator = nullptr
 																									);
 
 		private:
